ee024cb53e12558f9924f25552b5443ee391ea8f
Stan <dimitrov.s.dev@gmail.com>
2023-09-05 22:35:16 +0300

    SQL_Practice

---
+++ 1.Questions 1-5/Question_1.sql
@@ -0,0 +1,39 @@
+--a. How many Employees exist in the Pubs database?
+
+--b. How many Employees have a middle initial?
+
+--c. Based on hire date what is the first and last name of the newest employee?
+
+
+SELECT
+	*
+FROM
+	Employee
+
+-- a
+
+SELECT
+	COUNT(*) AS EmployeeCount
+FROM
+	Employee
+
+--b
+
+SELECT
+	COUNT(MiddleInitial) AS MiddleInitial
+FROM
+	Employee
+WHERE
+	MiddleInitial IS NOT NULL
+
+-- c
+
+SELECT
+	TOP 1
+	FirstName,
+	LastName
+FROM
+	Employee
+ORDER BY
+	Hire_Date DESC
+
